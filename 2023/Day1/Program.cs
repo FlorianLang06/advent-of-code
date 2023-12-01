2f9c81e0332ea9bef150e0ca6a247becd4182495
Florian Lang <144521337+FlorianLang06@users.noreply.github.com>
2023-12-01 08:01:37 +0000

    feat: Day 1 Part 1

--- 2023/Day1/Program.cs
+++ 2023/Day1/Program.cs
@@ -1,1 +1,30 @@
 Console.WriteLine("###### Advent of Code 2023 Day 1 ######\n");
+
+using var reader = new StreamReader("./input.txt");
+
+var endResult = 0;
+
+while (!reader.EndOfStream) {
+    var line = reader.ReadLine();
+    if (line == null) {
+        break;
+    }
+    char firstDigit = '\0';
+    char lastDigit = '\0';
+    foreach (var c in line.ToCharArray()) {
+        if (char.IsNumber(c)) {
+            if (firstDigit == '\0') {
+                firstDigit = c;
+            } else {
+                lastDigit = c;
+            }
+        }
+    }
+    string stringResult = string.Empty;
+    stringResult += firstDigit;
+    stringResult += lastDigit == '\0' ? firstDigit : lastDigit;
+    var lineResult = Int32.Parse(stringResult);
+    endResult += lineResult;
+}
+
+Console.WriteLine(endResult);
